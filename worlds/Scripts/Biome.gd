b190e26496076fe94a60316d12b60367edff8b75
Sundrop <random90s@gmail.com>
2025-01-31 21:50:59 +0800

    Relegate [Biome Prefab management to World Script] + Add [Grass Biome]

--- worlds/Scripts/Biome.gd
+++ worlds/Scripts/Biome.gd
@@ -1,3 +1,17 @@
 extends Node;
 
 enum Type {Earth, Grass};
+
+@onready var earthBiome:PackedScene = preload("res://Prefabs/Earth-Biome.tscn");
+@onready var grassBiome:PackedScene = preload("res://Prefabs/Grass-Biome.tscn");
+
+func Get_BiomePrefab(type:Type) -> PackedScene:
+	
+	match type:
+		Type.Earth:
+			return earthBiome;
+		Type.Grass:
+			return grassBiome;
+			
+	InGameDebugger.Warn("Failed to get Biome.");
+	return null;
