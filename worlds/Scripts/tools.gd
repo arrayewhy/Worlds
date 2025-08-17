a449e5e14f3597432e1de8765dac39ec5f6a8877
Sundrop <random90s@gmail.com>
2025-08-18 00:34:34 +0800

    Update [tools.gd]
    1. Added Function to get On-Grid Positions around a target Position
    2. Functions now grab World.CellSize automatically

--- worlds/Scripts/tools.gd
+++ worlds/Scripts/tools.gd
@@ -1,7 +1,27 @@
 class_name Tools
-	
-static func Float_OnGrid(val:float, cellSize:float) -> float:
-	return floor(val / cellSize) * cellSize;
 
-static func Coord_OnGrid(v2:Vector2, cellSize:float) -> Vector2:
-	return floor(v2 / cellSize) * cellSize;
+	
+static func Float_OnGrid(val:float) -> float:
+	return floor(val / World.CellSize) * World.CellSize;
+
+
+static func Coord_OnGrid(v2:Vector2) -> Vector2:
+	return floor(v2 / World.CellSize) * World.CellSize;
+
+
+static func V2_Array_Around(pos:Vector2, range:int, skipCenter:bool = false) -> Array[Vector2]:
+	
+	var p:Array[Vector2] = [];
+	
+	var width:float = range * 2 + 1;
+	
+	var start:Vector2 = pos + (-Vector2.ONE * range * World.CellSize);
+	
+	for y in width:
+		for x in width:
+			p.append(start + Vector2(x, y) * World.CellSize);
+	
+	if skipCenter:
+		p.remove_at(width * range + range);
+	
+	return p;
