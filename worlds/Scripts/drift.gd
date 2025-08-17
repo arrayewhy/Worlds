90fb09ea5aafa0b470fcbed37974fdeea7ce71c8
Sundrop <random90s@gmail.com>
2025-08-18 00:44:07 +0800

    Add [Message Bottle Script]

---
+++ worlds/Scripts/drift.gd
@@ -0,0 +1,34 @@
+class_name Drift extends Node
+
+var _master:Node2D;
+
+var _time:float;
+var _moveThresh:float = 2;
+
+
+func _ready() -> void:
+	_master = get_parent();
+	print(get_node("/root/map_generator"));
+
+
+func _process(delta: float) -> void:
+	_time += delta;
+	
+	if _time < _moveThresh:
+		return;
+	
+	_time = 0;
+	_Move_Right();
+
+
+func _Move_Right() -> void:
+	var hideTween:Tween = create_tween();
+	hideTween.tween_property(_master, "modulate:a", 0, 1);
+	
+	await hideTween.finished;
+	
+	var currX = Tools.Float_OnGrid(_master.position.x);
+	_master.position.x = currX + World.CellSize;
+	
+	var showTween:Tween = create_tween();
+	showTween.tween_property(_master, "modulate:a", 1, 1);
