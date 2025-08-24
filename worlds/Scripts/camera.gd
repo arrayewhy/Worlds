02f56d1b1aa9fd88512c10b10553dca4300cf8ea
Sundrop <random90s@gmail.com>
2025-08-25 00:55:45 +0800

    Update [camera.gd] 1. Added functions for changing Movement Speed (for when entering FOREST) 2. Made #TEMPORARY adjustments to account for Player

--- worlds/Scripts/camera.gd
+++ worlds/Scripts/camera.gd
@@ -3,7 +3,7 @@ extends Camera2D
 @export var _hide_on_zoom:Node2D;
 @export var _show_on_zoom:Node2D;
 
-@export var _cursor:Sprite2D;
+@export var _targObj:Sprite2D;
 
 const _normSpeed:float = 256;
 var _currSpeed:float = _normSpeed;
@@ -12,7 +12,13 @@ var _zoomed:bool;
 
 var _targPos:Vector2;
 
+var _speedTween:Tween;
+
 signal Zoom(on:bool, camTargPos:Vector2);
+# TEMPORARY: For Positioning the Player under the camera on start
+signal Cam_Pos_On_MapGenComplete(camPos:Vector2);
+
+@export var _debug:bool;
 
 
 # Functions: Built-in ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -43,7 +49,7 @@ func _process(delta: float) -> void:
 	
 	else:
 		
-		self.position += (_cursor.position - self.position) * delta * (_currSpeed / 128);
+		self.position += (_targObj.position - self.position) * delta * (_currSpeed / 128);
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
@@ -66,7 +72,7 @@ func _Zoom_Out() -> void:
 	
 		_zoomed = false;
 		
-		_targPos = _cursor.position;
+		_targPos = _targObj.position;
 		
 		Zoom.emit(_zoomed, _targPos);
 		
@@ -104,7 +110,32 @@ func _Zoom_In() -> void:
 		var zoomTween:Tween = create_tween();
 		zoomTween.set_trans(Tween.TRANS_CUBIC);
 		zoomTween.set_ease(Tween.EASE_IN_OUT);
-		zoomTween.tween_property(self, "zoom", Vector2(3, 3), 1.25);
+		zoomTween.tween_property(self, "zoom", Vector2(3, 3), 2.5);
+
+
+# Functions: Get Set ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+func Is_Zoomed(callerPath:String) -> bool:
+	if _debug: print("\ncamera.gd - Is_Zoomed, called by: ", callerPath);
+	return _zoomed;
+
+
+func Slow_CamSpeed(callerPath:String) -> void:
+	if _debug: print("\ncamera.gd - Slow_CamSpeed, called by: ", callerPath);
+	if _speedTween && _speedTween.is_running():
+		_speedTween.stop();
+	_speedTween = create_tween();
+	_speedTween.tween_property(self, "_currSpeed", _normSpeed / 32, 1);
+
+
+func Reset_CamSpeed(callerPath:String) -> void:
+	if _debug: print("\ncamera.gd - Reset_CamSpeed, called by: ", callerPath);
+	if _speedTween && _speedTween.is_running():
+		_speedTween.stop();
+	_speedTween = create_tween();
+	_speedTween.tween_property(self, "_currSpeed", _normSpeed, 1);
+
 
 # Functions: Signals ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
@@ -113,3 +144,4 @@ func _On_Initial_MapGen_Complete() -> void:
 	self.position = Vector2.ONE * World.CellSize * (World.MapWidth() / 2);
 	_targPos = self.position;
 	#_Zoom_In();
+	Cam_Pos_On_MapGenComplete.emit(self.position);
