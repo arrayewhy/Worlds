1212c1677c5f47a014a8fcf322656cf7c0ccdb56
Sundrop <random90s@gmail.com>
2026-03-29 14:59:58 +0800

    Added Marking Fading on Mouseover
    Note: Previous commit Confused cursor.gd with mouse.gd

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
-@export var _mapGen:Node2D;
+@onready var _mapGen:Node = $"../map_generator";
 
 #var _terrain_lifters:Dictionary;
+
+var _last_mouse_pos:Vector2;
+
+var _hover_timer:float;
+var _curr_hover_coord:Vector2 = Vector2.INF;
+
+var _skip_fade:bool;
 
 #TEMPORARY
 # Why do we need this signal?
@@ -13,48 +20,101 @@ signal Left_Click(mouse_coord:Vector2);
 # Functions: Built-in ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	
+	var mouse_pos:Vector2 = get_global_mouse_position();
 	
 	if Input.is_action_just_pressed("Left-Click"):
 		
 		Left_Click.emit(get_global_mouse_position());
 		
 		# Play Ship Horn Sound on Click
-		
-		var gPos:Vector2 = World.Coord_OnGrid(get_global_mouse_position());
-		var idx:int = World.Convert_Coord_To_Index(gPos);
+		var mouse_grid_pos:Vector2 = World.Coord_OnGrid(get_global_mouse_position());
+		var idx:int = World.Convert_Coord_To_Index(mouse_grid_pos);
 		if _mapGen.Get_Marking(idx, self.get_path()) == Map_Data.Marking.BOAT:
 			SoundMaster.Ship_Horn();
+			
+	if _curr_hover_coord == Vector2.INF:
+		if mouse_pos.distance_to(_last_mouse_pos) < 2:
+			_hover_timer += delta;
+		else:
+			_hover_timer = 0;
+	else:
 		
-		# Get Mouse Position on Grid
-		#var mouse_coord:Vector2 = World.Coord_OnGrid(get_global_mouse_position());
+		if mouse_pos.distance_to(_curr_hover_coord) > World.CellSize * .75:
+
+			# Reset Detail or Secret
+			_Reset_Reveal(_curr_hover_coord);
+			
+			_curr_hover_coord = Vector2.INF;
+	
+	# Reveal Detail or Secret
+	
+	if _hover_timer >= .2:
+		
+		_hover_timer = 0;
+		
+		_curr_hover_coord = World.Coord_OnGrid(mouse_pos);
+		
+		_Reveal(_curr_hover_coord);
+	
+	_last_mouse_pos = mouse_pos;
 
 
-# Lifter ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+func _Reveal(coord:Vector2) -> void:
+	
+	var secret_spr:Sprite2D = _mapGen.Get_Spr_Secret(coord, self.get_path());
+	if secret_spr:
+		
+		# Secret
+		Fader.Fade(secret_spr, Fader.Phase.IN, .5);
+		
+		# TEMPORARY: Mountain Dragon Growl
+		var secret_type:Map_Data.Secrets = _mapGen.Get_Secret(World.Convert_Coord_To_Index(coord), self.get_path());
+		if secret_type == Map_Data.Secrets.DRAGON:
+			SoundMaster.Dragon_Growl();
+		
+		# Detail
+		var detail_spr:Sprite2D = _mapGen.Get_Spr_Detail(coord, self.get_path());
+		if detail_spr:
+			Fader.Fade(detail_spr, Fader.Phase.OUT, .5);
+		# Terrain
+			var terrain_spr = _mapGen.Get_TerrainSprite(coord, self.get_path());
+			Fader.Colour_Fade(terrain_spr, Color.BLACK, .5);
+	else:
+		var detail_spr:Sprite2D = _mapGen.Get_Spr_Detail(coord, self.get_path());
+		if detail_spr:
+			# Some Details like Peaks always start Visible,
+			# so we Skip them.
+			if detail_spr.modulate.a >= 1:
+				_skip_fade = true;
+			else:
+				Fader.Fade(detail_spr, Fader.Phase.IN, .5);
+				# Marking
+				var marking_spr:Sprite2D = _mapGen.Get_Spr_Marking(coord, self.get_path());
+				Fader.Fade(marking_spr, Fader.Phase.OUT, .5);
 
 
-#func _Do_Terrain_Lifter(mouse_coord:Vector2) -> void:
-	#if _terrain_lifters.has(mouse_coord):
-		#return;
-	#
-	#var treasureType:int = _mapGen.Get_Buried(World.Convert_Coord_To_Index(mouse_coord), self.get_path());
-	#
-	#if treasureType < 0:
-		#return;
-	#
-	#var spr:Sprite2D = _mapGen.Get_TerrainSprite(mouse_coord, self.get_path());
-	#
-	#if !spr:
-		#return;
-	#
-	#var lifter:Terrain_Lifter = Terrain_Lifter.new(spr, mouse_coord, treasureType, get_global_mouse_position().y);
-	#self.add_child(lifter);
-	#
-	## Record Terrain Lifter
-	#_terrain_lifters.set(mouse_coord, lifter);
-	#
-	#lifter.Remove.connect(_Remove_Terrain_Lifter);
-
-
-#func _Remove_Terrain_Lifter(ori_pos:Vector2) -> void:
-	#_terrain_lifters.erase(ori_pos);
+func _Reset_Reveal(coord:Vector2) -> void:
+	
+	var secret_spr:Sprite2D = _mapGen.Get_Spr_Secret(coord, self.get_path());
+	if secret_spr:
+		# Secret
+		Fader.Fade(secret_spr, Fader.Phase.OUT, .5);
+		# Detail
+		var detail_spr:Sprite2D = _mapGen.Get_Spr_Detail(coord, self.get_path());
+		if detail_spr:
+			Fader.Fade(detail_spr, Fader.Phase.IN, .5);
+		# Terrain
+		var terrain_spr = _mapGen.Get_TerrainSprite(coord, self.get_path());
+		Fader.Colour_Fade(terrain_spr, Color.WHITE, .5);
+	else:
+		var detail_spr:Sprite2D = _mapGen.Get_Spr_Detail(coord, self.get_path());
+		if detail_spr:
+			if _skip_fade:
+				_skip_fade = false;
+			else:
+				Fader.Fade(detail_spr, Fader.Phase.OUT, .5);
+				# Marking
+				var marking_spr:Sprite2D = _mapGen.Get_Spr_Marking(coord, self.get_path());
+				Fader.Fade(marking_spr, Fader.Phase.IN, .5);
