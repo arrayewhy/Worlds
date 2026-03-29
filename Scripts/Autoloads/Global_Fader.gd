0f4e8784124e19f2cad657e873d7e25d02123c23
Sundrop <random90s@gmail.com>
2026-03-29 14:53:38 +0800

    Add Global Fader to Replace existing fader

---
+++ Scripts/Autoloads/Global_Fader.gd
@@ -0,0 +1,66 @@
+extends Node
+
+enum Phase { Null, IN, OUT }
+
+var _active_fades:Dictionary[int, Array]; # Node ID : [ Node, Tween, Phase/Colour ]
+
+
+func Fade(obj:Node2D, phase:Phase, duration:float) -> void:
+	
+	var id:int = obj.get_instance_id();
+	
+	# If the Node is already fading in the Same Phase, do Nothing.
+	# Or else, Stop and Replace the Active Tween.
+	if _active_fades.has(id):
+		if _active_fades[id][2] == phase:
+			return;
+		else:
+			_active_fades[id][1].kill();
+	
+	# Start Fade In Tween
+	var tween:Tween = create_tween();
+	
+	var targ_alpha:float;
+	
+	match phase:
+		Phase.IN:
+			targ_alpha = 1;
+		Phase.OUT:
+			targ_alpha = 0;
+		_:
+			printerr("Fade Phase is Null for: ", obj.get_path());
+			targ_alpha = 0;
+	
+	tween.tween_property(obj, "modulate:a", targ_alpha, duration);
+	
+	# Record Fade Parameters
+	_active_fades.set(id, [obj, tween, phase]);
+	
+	await tween.finished;
+	
+	_active_fades.erase(id);
+
+
+func Colour_Fade(obj:Node2D, col:Color, duration:float) -> void:
+	
+	var id:int = obj.get_instance_id();
+	
+	# If the Node is already fading in the Same Phase, do Nothing.
+	# Or else, Stop and Replace the Active Tween.
+	if _active_fades.has(id):
+		if _active_fades[id][2] == col:
+			return;
+		else:
+			_active_fades[id][1].kill();
+	
+	# Start Fade In Tween
+	var tween:Tween = create_tween();
+	
+	tween.tween_property(obj, "modulate", col, duration);
+	
+	# Record Fade Parameters
+	_active_fades.set(id, [obj, tween, col]);
+	
+	await tween.finished;
+	
+	_active_fades.erase(id);
