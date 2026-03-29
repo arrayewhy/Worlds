35f7ea0ba9e99113803e5b38eb10aea41ce4b5b8
Sundrop <random90s@gmail.com>
2026-03-29 15:01:18 +0800

    Add Dragon Growl

--- Scripts/Autoloads/Sound-Master.gd
+++ Scripts/Autoloads/Sound-Master.gd
@@ -2,11 +2,12 @@ extends Node
 
 const _sounds:Dictionary[String, Object] = {
 	"DOG_BARK" : preload("res://Audio/SFX/Dog-Bark_700748__robinhood76__11780-labrador-dog-barks.wav"),
-	"SHIP_HORN" : preload("res://Audio/SFX/157284_Ship_Horn.wav")
+	"SHIP_HORN" : preload("res://Audio/SFX/157284_Ship_Horn.wav"),
+	"DRAGON_GROWL" : preload("res://Audio/SFX/Dragon_Growl.mp3"),
 }
 
 
-func _Play_Sound(sound:Object, vol:float, rand_pitch:bool = false) -> void:
+func _Play_Sound(sound:Object, decibels:float, rand_pitch:bool = false) -> void:
 	
 	# Play sound with AudioStreamPlayer that is NOT Busy
 	
@@ -16,7 +17,9 @@ func _Play_Sound(sound:Object, vol:float, rand_pitch:bool = false) -> void:
 			aud_player.stream = sound;
 			
 			if rand_pitch:
-				aud_player.pitch_scale = randf_range(-.1, .1);
+				aud_player.pitch_scale = randf_range(1, 1.1);
+			else:
+				aud_player.pitch_scale = 1;
 			
 			aud_player.play();
 			return;
@@ -29,10 +32,12 @@ func _Play_Sound(sound:Object, vol:float, rand_pitch:bool = false) -> void:
 	self.add_child(new_aud_player);
 	
 	new_aud_player.stream = sound;
-	new_aud_player.volume_db = vol;
+	new_aud_player.volume_db = decibels;
 	
 	if rand_pitch:
-		new_aud_player.pitch_scale = randf_range(-1, 1);
+		new_aud_player.pitch_scale = randf_range(1, 1.1);
+	else:
+		new_aud_player.pitch_scale = 1;
 	
 	new_aud_player.play();
 
@@ -40,8 +45,11 @@ func _Play_Sound(sound:Object, vol:float, rand_pitch:bool = false) -> void:
 # Public ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
-func Ship_Horn(vol:float = 0) -> void:
-	_Play_Sound(_sounds.get("SHIP_HORN"), vol, true);
+func Ship_Horn(decibels:float = 0) -> void:
+	_Play_Sound(_sounds.get("SHIP_HORN"), decibels, true);
 
-func Dog_Bark(vol:float = 0) -> void:
-	_Play_Sound(_sounds.get("DOG_BARK"), vol, true);
+func Dog_Bark(decibels:float = 0) -> void:
+	_Play_Sound(_sounds.get("DOG_BARK"), decibels, true);
+
+func Dragon_Growl(decibels:float = 0) -> void:
+	_Play_Sound(_sounds.get("DRAGON_GROWL"), decibels, true)
