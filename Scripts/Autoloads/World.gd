0eae66f5b503a2f7df8f55a3324ceac87aea2137
Sundrop <random90s@gmail.com>
2026-04-10 13:18:09 +0800

    Updated World: 1. Relegate Scene Change to Scene_Changer.gd 2. Relegate Coord, Idx, Data Conversion to Tools Class 3. Relegate Sprite Creation to sprite_handler.gd

--- Scripts/Autoloads/World.gd
+++ Scripts/Autoloads/World.gd
@@ -1,32 +1,16 @@
 extends Node;
 
-const _spriteSheet:Texture2D = preload("res://Sprites/sparks_in_the_dark.png");
-
 const CellSize:float = 16;
 const Spr_Reg_Size:float = 256;
-var _mapWidth:int;
+var MAP_UNIT_WIDTH:int;
 
-var _player_coord:Vector2;
+var PLAYER_COORD:Vector2;
 
 #signal Initial_MapGen_Complete;
 
 signal Replace_Terrain(idx:int, type:Map_Data.Terrain, spr:Sprite2D);
 
 var _debug:bool;
-
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Cancel"):
-		$"../Main/Curtain".Obscure();
-		$"../Main/Curtain".Fade_Complete.connect(_Quit);
-		
-
-
-# Functions: Signals ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
-
-func _Quit() -> void:
-	get_tree().quit();
 
 
 # Player ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -37,41 +21,23 @@ func Set_Player_Coord(coord:Vector2, callerPath:String) -> void:
 	if _debug: print("\nWorld.gd - Set_Player_Coord, called by: ", callerPath);
 
 func _Set_Player_Coord(coord:Vector2) -> void:
-	_player_coord = coord;
+	PLAYER_COORD = coord;
 
 
-func Player_Coord() -> Vector2:
-	return _player_coord;
+# Functions ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
-func Distance_From_Player(pos:Vector2) -> float:
-	return _player_coord.distance_to(pos);
-
-
-#func Near_Player(pos:Vector2) -> bool:
-	#return _player_coord.distance_to(pos) <= CellSize;
-
-
-#func Signal_Initial_MapGen_Complete(callerPath:String) -> void:
-	#if _debug: print("\nWorld.gd - Signal_Initial_MapGen_Complete, called by: ", callerPath);
-	#Initial_MapGen_Complete.emit();
-
-
-func Signal_Replace_Terrain_Sprite(idx:int, type:Map_Data.Terrain, spr:Sprite2D, callerPath:String) -> void:
-	if _debug: print("\nWorld.gd - Signal_Replace_Terrain_Sprite, called by: ", callerPath);
+func Replace_Terrain_Sprite(idx:int, type:Map_Data.Terrain, spr:Sprite2D, callerPath:String) -> void:
+	if _debug: print("\nWorld.gd - Replace_Terrain_Sprite, called by: ", callerPath);
 	Replace_Terrain.emit(idx, type, spr);
 
 
-# Functions: Get Set ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# Get Set ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
-func Set_MapWidth_In_Units(width:int, callerPath:String) -> void:
-	if _debug: print("\nWorld.gd - Set_MapWidth_In_Units, called by: ", callerPath);
-	_mapWidth = width;
-
-
-func MapWidth_In_Units() -> int:
-	return _mapWidth;
+func Set_Map_Width_In_Units(width:int, callerPath:String) -> void:
+	if _debug: print("\nWorld.gd - Set_Map_Width_In_Units, called by: ", callerPath);
+	MAP_UNIT_WIDTH = width;
 
 
 #func Set_TotalUnitWidth(width:float, callerPath:String) -> void:
@@ -81,85 +47,3 @@ func MapWidth_In_Units() -> int:
 #
 #func Total_Unit_Width() -> float:
 	#return _totalUnitWidth;
-
-
-func Float_OnGrid(val:float) -> float:
-	return floor(val / CellSize) * CellSize;
-
-
-func Coord_OnGrid(v2:Vector2) -> Vector2:
-	return round(v2 / CellSize) * CellSize;
-	#return floor(v2 / CellSize) * CellSize;
-
-
-func Convert_Index_To_Coord(idx:int) -> Vector2:
-	return Vector2(idx % _mapWidth, idx / _mapWidth) * CellSize;
-
-
-func Convert_Coord_To_Index(coord:Vector2) -> int:
-	#var float_idx:float = (_mapWidth * coord.y + coord.x) / CellSize;
-	#return int(float_idx);
-	return (_mapWidth * coord.y + coord.x) / CellSize;
-
-
-func V2_Array_Around(pos:Vector2, radius:int, skipCenter:bool = false) -> Array[Vector2]:
-	
-	var p:Array[Vector2] = [];
-	
-	var length:float = radius * 2 + 1;
-	
-	var start:Vector2 = pos + (-Vector2.ONE * radius * CellSize);
-	#REFACTOR
-	for y in length:
-		
-		if start.y + y * CellSize < 0 || start.y + y * CellSize >= _mapWidth * CellSize:
-			continue;
-		
-		for x in length:
-			
-			if start.x + x * CellSize < 0 || start.x + x * CellSize >= _mapWidth * CellSize:
-				continue;
-			p.append(start + Vector2(x, y) * CellSize);
-	
-	if skipCenter && p.has(pos):
-		p.erase(pos);
-		#p.remove_at(length * radius + radius);
-	
-	return p;
-
-
-func Convert_CoordArray_To_IdxArray(coordArray:Array[Vector2]) -> Array[int]:
-	var idx_array:Array[int];
-	for coord in coordArray:
-		var idx:int = int((_mapWidth * coord.y + coord.x) / CellSize);
-		idx_array.push_back(idx);
-	return idx_array;
-
-
-func Terrain_Is_Land(terrain:Map_Data.Terrain) -> bool:
-	if terrain == Map_Data.Terrain.MOUNTAIN \
-	|| terrain == Map_Data.Terrain.MOUNTAIN_PATH \
-	|| terrain == Map_Data.Terrain.FOREST \
-	|| terrain == Map_Data.Terrain.GROUND \
-	|| terrain == Map_Data.Terrain.SHORE \
-	|| terrain == Map_Data.Terrain.TEMPLE_BROWN \
-	|| terrain == Map_Data.Terrain.DOCK:
-		return true;
-	return false;
-
-
-func Create_Sprite(regPosX:float, regPosY:float, cells_y:int = 1, tex:Texture2D = _spriteSheet) -> Sprite2D:
-	var spr:Sprite2D = Sprite2D.new();
-	spr.texture = tex;
-	spr.region_enabled = true;
-	spr.region_rect.size = Vector2(World.Spr_Reg_Size, World.Spr_Reg_Size);
-	spr.region_rect.position = Vector2(regPosX, regPosY) * World.Spr_Reg_Size;
-	spr.scale /= World.CellSize;
-	
-	spr.region_rect.size = Vector2(World.Spr_Reg_Size, World.Spr_Reg_Size * cells_y);
-	
-	if cells_y > 1:
-		var displace_units:float = float(cells_y) / 2;
-		spr.offset.y -= World.CellSize * 8 * displace_units;
-	
-	return spr;
